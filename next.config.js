9fe2f14523a98a33a8fcdf01d54bdb5e554334d3
Youngo356 <youngonwunatuegwu@gmail.com>
2026-05-16 04:41:24 +0100

    Update Next.js config for static export

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    allowMiddlewareRedirects: true,
   },
 }
 
